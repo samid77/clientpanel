ce64a8a6d163c5c1a6d16f7505698d28ea4a4038
Samid77 <dimaseptyanto@windowslive.com>
2019-02-02 22:34:10 +0700

    Client balance update

--- src/app/components/client-details/client-details.component.ts
+++ src/app/components/client-details/client-details.component.ts
@@ -35,4 +35,8 @@ export class ClientDetailsComponent implements OnInit {
     });
   }
 
+  updateBalance() {
+    this.clientService.updateClient(this.client);
+    this.flashMessage.show('Balance updated', {cssClass: 'alert-success', timeout: 4000});
+  }
 }
